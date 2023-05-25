0bff0cf2ca357fdea5ae67dd5c56b2a068249cc5
ophelia-phan <115785294+ophelia-phan@users.noreply.github.com>
2023-05-25 17:11:48 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -96,7 +96,7 @@ class Thread(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     titre = db.Column(db.String(100))
     content = db.Column(db.Text)
-    anonyme = db.Column(db.Boolean, default=False)
+    anonyme = db.Column(db.String(8), default=False)
     id_user = db.Column(db.Integer, db.ForeignKey('user.id'))
     date_pub = db.Column(db.DateTime, default=db.func.now())
     
@@ -138,6 +138,7 @@ class Article(db.Model):
     titre = db.Column(db.String(100), nullable = False)
     contenu = db.Column(db.Text, nullable = False)
     categorie_id = db.Column(db.Integer, db.ForeignKey('categorie.id'), nullable=False)
+    
     def __init__(self, titre, contenu, categorie_id):
         self.titre = titre
         self.contenu = contenu
@@ -220,7 +221,7 @@ with app.app_context():
 def home():
     return render_template("home.html")
 
-@app.route("/login", methods=["GET"])
+@app.route("/login")
 def login():
     return render_template("login.html")
     
@@ -236,7 +237,9 @@ def register():
     therapeute = Therapeute.query.filter_by(email=email).first()
     
     if (user is not None) and user.password == password:        
-        session["user_id"] = user.id
+        session["name"] = user.name
+        session["email"] = user.email
+        
         if remember_me:
             # Si la case "Se souvenir de moi" est cochée, définit un cookie pour se souvenir de l'utilisateur pendant 30 jours
             session.permanent = True
@@ -244,7 +247,9 @@ def register():
         return redirect(url_for("home"))
     
     elif (therapeute is not None) and therapeute.password == password :
-        session["therapeute_id"] = therapeute.id
+        session["name"] = therapeute.name
+        session["email"] = therapeute.email
+        
         if remember_me:
             # Si la case "Se souvenir de moi" est cochée, définit un cookie pour se souvenir de l'utilisateur pendant 30 jours
             session.permanent = True
@@ -267,9 +272,9 @@ def create_account():
     email = request.form["email"]
     password = request.form["password"]
     user = User(name, email, password, False)
-    session["name"] = request.form["name"]
+    
     session["email"] = request.form["email"]
-    session["password"] = request.form["password"]
+    session["name"] = request.form["name"]
     
     db.session.add(user)
     db.session.commit()
@@ -308,36 +313,32 @@ def create_pro_account():
     phototype = photo_profil.mimetype
     
     img = Image(img=photo_profil.read(), mimetype=phototype, name=filename, username=name)
-    print("marche ?")
+    
     db.session.add(img)
     db.session.commit()
-    print("je suis dans la base")
+    
     pro = Therapeute(name, email, password, specialite, description,max_sessions, nb_experience, formation)
-
-    print("j'ai créé un user therapeute")
     
     #pro.photo_profil = img
     
     session["name"] = request.form["name"]
     session["email"] = request.form["email"]
-    session["password"] = request.form["password"]
+    
     
     db.session.add(pro)
     db.session.commit()
     return redirect(url_for("home"))
 
 
-
-    
+##################################################################################################################################################
+#LOG OUT    
 
 @app.route("/logout")
 def logout():
+    session.pop("email", None)
     session.pop("name", None)
     return redirect(url_for("home"))
 
-@app.route("/articles")
-def display_articles():
-    return render_template("articles.html")
 
 
 ##################################################################################################################################################
@@ -345,20 +346,22 @@ def display_articles():
 
 @app.route("/user/<int:user_id>")
 def profil(user_id):
-    name = session["name"]
     email = session["email"]
+    name=session["name"]
     user = User.query.get(user_id)
     
-    return render_template("user.html", name=name, email=email)
+    return render_template("user.html", email=email, user=user)
 
 @app.route("/update-profile/<int:user_id>", methods=["POST"])
 def update_profile(user_id):
     new_name = request.form["name"]
     new_email = request.form["email"]
+    new_password = request.form["password"]
     user_object = User.query.get(user_id)
     
     user_object.name = new_name
     user_object.email = new_email
+    user_object.password = new_password
     
     db.session.commit()
     return redirect(url_for("home"))
@@ -369,22 +372,34 @@ def update_profile(user_id):
 
 @app.route("/therapeute/<int:therapeute_id>")
 def profil_pro(therapeute_id):
-    name = session["name"]
     email = session["email"]
+    name=session["name"]
     therapeute = Therapeute.query.get(therapeute_id)
-    photo_profil = therapeute.photo_profil
     
-    return render_template("user_pro.html", name=name, email=email, photo_profil=photo_profil)
+    return render_template("user_pro.html", email=email, therapeute=therapeute)
 
 @app.route("/update-profile-pro/<int:therapeute_id>",methods=["POST"])
 def update_profile_pro(therapeute_id):
     new_name = request.form["name"]
     new_email = request.form["email"]
+    new_password = request.form["password"]
+    new_specialite = request.form["specialite"]
+    new_description = request.form["description"]
+    new_max_sessions = request.form["max_sessions"]
+    new_nb_experience = request.form["nb_experience"]
+    new_formation = request.form["formation"]
+    
     
     therapeute_object = Therapeute.query.get(therapeute_id)
     
     therapeute_object.name = new_name
     therapeute_object.email = new_email
+    therapeute_object.password = new_password
+    therapeute_object.specialite = new_specialite
+    therapeute_object.description = new_description
+    therapeute_object.max_sessions = new_max_sessions
+    therapeute_object.nb_experience = new_nb_experience
+    therapeute_object.formation = new_formation
     
     db.session.commit()
     return redirect(url_for("home"))
@@ -392,12 +407,13 @@ def update_profile_pro(therapeute_id):
 @app.route("/upload-profile-picture/<int:therapeute_id>", methods=["POST"])
 def upload_profile_picture(therapeute_id):
     name = session["name"]
+    email=session["email"]
     new_photo = request.files["photo_profil"]
     if not new_photo:
         return "No pic uploaded",400 #error bad request
     filename=secure_filename(new_photo.filename)
     phototype = new_photo.mimetype
-    new_photo_profil = Img(new_photo.read(),phototype, filename, name)
+    new_photo_profil = Image(new_photo.read(),phototype, filename, name)
     
     therapeute_object = Therapeute.query.get(therapeute_id)
     therapeute_object.photo_profil = new_photo_profil
@@ -414,24 +430,24 @@ def forum():
     threads = Thread.query.all()
     name = session["name"]
     email = session["email"]
-    return render_template("forum.html", all_threads = threads, name=name, email=email)
+    return render_template("forum.html", all_threads = threads)
 
-@app.route("/add-thread",methods=["POST"])
+@app.route("/add-thread",methods=["GET","POST"])
 def add_thread():
     return render_template("new_thread.html")
 
 @app.route("/new-thread", methods =["POST"])
 def new_thread():
-    titre = request.form["titre"]
-    contenu = request.form["contenu"]
-    anonyme=request.form["anonyme"]
+    titre = request.form["topic"]
+    contenu = request.form["description"]
+    anonyme = request.form["anonyme"]
     author = session["name"]
-    date_pub = datetime.utcnow
+    date_pub = datetime.now()
     
     thread = Thread(titre, contenu, anonyme, author, date_pub)
     db.session.add(thread)
     db.session.commit()
-    return redirect(url_for("forum")) #à changer pour le redirect vers la page du thread créé si possible
+    return redirect(url_for("thread", thread_id = thread.id)) #à changer pour le redirect vers la page du thread créé si possible
 
 
 @app.route("/delete-thread/<int:comment_id>")
@@ -454,7 +470,7 @@ def change_thread(thread_id):
     thread_object.title = new_title
     
     db.session.commit()
-    return redirect(url_for("forum"))
+    return redirect(url_for("thread", thread_id = thread_object.id))
 
 
 ##################################################################################################################################################
@@ -463,10 +479,9 @@ def change_thread(thread_id):
 @app.route("/thread/<int:thread_id>")
 def thread(thread_id):
     thread = Thread.query.get(thread_id)
-    comments = Comment.query.all(id_thread = thread_id)
+    comments = Comment.query.filter_by(id_thread = thread.id)
     name = session["name"]
-    email = session["email"]
-    return render_template("thread.html", thread = thread, all_comments=comments,name=name, email=email)
+    return render_template("thread.html", thread = thread, all_comments=comments,name=name)
 
 @app.route("/add-comment", methods=["POST"])
 def add_comment():
@@ -474,6 +489,7 @@ def add_comment():
     content = request.form["comment"]
     date = datetime.today()
     comment = Comment(author, content, date)
+    
     db.session.add(comment)
     db.session.commit()
     return redirect(url_for("thread"))
@@ -500,29 +516,32 @@ def new_comment(comment_id):
 
 
 ##################################################################################################################################################
-# Feature : Affichage des articles 
+# Feature : Affichage des articles
+
+@app.route("/articles")
 def articles():
     # Récupérer les articles depuis la base de données ou autre source de données
-    articles = [
-        {'titre': 'Article 1', 'contenu': 'Contenu de l\'article 1'},
-        {'titre': 'Article 2', 'contenu': 'Contenu de l\'article 2'},
-        {'titre': 'Article 3', 'contenu': 'Contenu de l\'article 3'}
-    ]
+    articles = Article.query.all()
+    liste_articles = []
     
-    return render_template('articles.html', articles=articles)
+    for article in articles:
+        article_dico = {'titre': article.titre , 'contenu': article.contenu}
+        liste_articles.append(article_dico)
+        
+    return render_template('articles.html', articles=liste_articles)
 
-# Route pour afficher un article spécifique
 @app.route('/articles/<int:article_id>')
 def article(article_id):
-    # Récupérer l'article avec l'ID spécifié depuis la base de données ou autre source de données
-    article = {'titre': 'Article', 'contenu': 'Contenu de l\'article'}
-    
-    return render_template('article.html', article=article)
+    article = Article.query.get(article_id)
+    return render_template('article.html', article = article)
+
+
 ##################################################################################################################################################
 # Feature : Prise de rendez-vous 
 @app.route('/booking')
 def booking():
     return render_template('rdv.html')
+
 # Route pour enregistrer un rendez-vous
 @app.route('/booking', methods=['POST'])
 def booking_submit():
